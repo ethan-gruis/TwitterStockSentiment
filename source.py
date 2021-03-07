42c69849570c6ad1c5bd2fa3846c56bffb31df59
ethan-gruis <egruis98@gmail.com>
2021-03-07 13:55:53 -0500

    added function!

--- source.py
+++ source.py
@@ -1,34 +1,45 @@
-!python --version
-# !pip install twitterscraper
+# !python --version
 # !pip install git+https://github.com/JustAnotherArchivist/snscrape.git
+#!pip3 install --upgrade git+https://github.com/JustAnotherArchivist/snscrape@master
 
-https://github.com/JustAnotherArchivist/snscrape/issues/81 
-
-import twitterscraper
 import pandas as pd
 import re
 import os
+from sys import platform
 
 
-tweet_count = 500
 
+## EXAMPLE CODE
 
 # Using OS library to call CLI commands in Python
-os.system("snscrape --jsonl --max-results {} twitter-search '$GME'> user-tweets.json".format(tweet_count))
-# snscrape is some github package that helps you scrape twitter easily from cmd terminal
+# tweet_count = 500
+# os.system("snscrape --jsonl --max-results {} twitter-search '$GME'> user-tweets.json".format(tweet_count))
 
-os.system("snscrape --jsonl twitter-search '$GME since:2021-03-01'> tweets.json")
-# Reads the json generated from the CLI command above and creates a pandas dataframe
+# mac:
+# os.system("snscrape --jsonl twitter-search '$GME since:2021-03-01 until:2021-03-02'> tweets.json")
 
-gme_tweets = pd.read_json('tweets.json', lines=True)
-gme_tweets
+# windows:
+# os.system("snscrape --jsonl --max-results 500 --since 2020-02-01 twitter-search \"$GME until:2021-02-02\" > text-query-tweets.json")
 
-def callStock(stockname):
+def pullStockTweets(stock, since, until):
+    call = ""
+    if platform == "win32":
+        call = "snscrape --jsonl twitter-search \"" + str(stock) + " since:" + str(since) + " until:" + str(until) + "\"> function-tweets.json"
+        # do windows stuff
+        os.system(call)
+        print("done")
+    else:
+        call = "snscrape --jsonl twitter-search '" + str(stock) + " since:" + str(since) + " until:" + str(until) + "'> function-tweets.json"
+        os.system(call)
+        print("done")
+    return(pd.read_json('function-tweets.json', lines = True))
+        # do unix stuff
 
-
-https://github.com/JustAnotherArchivist/snscrape
+gme = pullStockTweets("$GME", "2021-02-14", "2021-02-17")
 
 # Creating finction to scrape stock ticker by name (done with snscrape)
+# Function needs to include windows, linux,or OSX
 # Formatting content to sentament able material
 #Look into date range specification
 #Look into plotting sentiments against stock reacton.
+gme.head()
