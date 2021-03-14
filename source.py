fdc45aba9b199ab950176267c3b158eec7572108
bsiglow <bsiglow@nd.edu>
2021-03-14 14:18:36 -0400

    Added function for overall stock ticker working to filter down by previously mentioned dates in function

--- source.py
+++ source.py
@@ -6,6 +6,7 @@ import pandas as pd
 import re
 import os
 from sys import platform
+from datetime import datetime
 
 
 
@@ -22,20 +23,23 @@ from sys import platform
 # os.system("snscrape --jsonl --max-results 500 --since 2020-02-01 twitter-search \"$GME until:2021-02-02\" > text-query-tweets.json")
 
 def pullStockTweets(stock, since, until):
+    stock = str(stock).upper()
+    stock_tweet = "$" + str(stock)
     call = ""
     if platform == "win32":
-        call = "snscrape --jsonl twitter-search \"" + str(stock) + " since:" + str(since) + " until:" + str(until) + "\"> function-tweets.json"
+        call = "snscrape --jsonl twitter-search \"" + str(stock_tweet) + " since:" + str(since) + " until:" + str(until) + "\"> function-tweets.json"
         # do windows stuff
         os.system(call)
         print("done")
     else:
-        call = "snscrape --jsonl twitter-search '" + str(stock) + " since:" + str(since) + " until:" + str(until) + "'> function-tweets.json"
+        call = "snscrape --jsonl twitter-search '" + str(stock_tweet) + " since:" + str(since) + " until:" + str(until) + "'> function-tweets.json"
         os.system(call)
         print("done")
     return(pd.read_json('function-tweets.json', lines = True))
         # do unix stuff
 
-gme = pullStockTweets("$GME", "2021-02-14", "2021-02-17")
+
+gme = pullStockTweets("gMe", "2021-02-14", "2021-02-17")
 
 # Creating finction to scrape stock ticker by name (done with snscrape)
 # Function needs to include windows, linux,or OSX
@@ -43,3 +47,18 @@ gme = pullStockTweets("$GME", "2021-02-14", "2021-02-17")
 #Look into date range specification
 #Look into plotting sentiments against stock reacton.
 gme.head()
+
+def StokTickerValue(stock):
+    stock_link = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + str(stock) + "&apikey=" + "R0GH7135BX9T3R6F" + "&datatype=csv&outputsize=full"
+    stock_value = pd.read_csv(str(stock_link))
+    return(stock_value)
+
+GME = StokTickerValue("GME")
+
+GME.head()
+
+type(GME.timestamp)
+datetime.strptime(GME.timestamp[1], '%Y-%m-%d')
+GME.close[GME.timestamp == maybe]
+maybe = pd.date_range(start="2019-09-09",end="2019-09-10")
+print(maybe)
